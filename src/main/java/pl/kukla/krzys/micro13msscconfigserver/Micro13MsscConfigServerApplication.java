4c4162bf056021b86bf97e48f9b173ad776170bd
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-22 14:23:37 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/micro13msscconfigserver/Micro13MsscConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.micro13msscconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro13MsscConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro13MsscConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/micro13msscconfigserver/Micro13MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.micro13msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro13MsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
